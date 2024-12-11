933aac5469ef3680dab8ef5d452c0f580ef9c3da
Stepan Greenberg <stepan.greenberg@ya.ru>
2024-12-11 13:12:05 +0300

    update

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -1,12 +1,26 @@
-import {DataSource} from "typeorm";
-import {config} from "./config";
-import {Task, User} from "../db/models";
+import { DataSource } from "typeorm";
+import { Task } from "../db/models/task.entity";
+import { User } from "../db/models/user.entity";
+import { config } from "./config";
 
+console.log("Creating DataSource with config:", {
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    database: "postgres"
+    // not logging password for security
+});
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: config.pg_uri,
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
     synchronize: true,
-    logging: false,
+    logging: true,
     entities: [Task, User],
+    migrations: [],
+    subscribers: [],
 });
